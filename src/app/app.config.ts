5ecef9d534f7d781c2e4eb743c2194bdfc0cea10
guillaumebeysson <guillaume.beysson@gmail.com>
2024-06-03 14:07:59 +0200

    fix bug animation in console

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideRouter } from '@angular/router';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
